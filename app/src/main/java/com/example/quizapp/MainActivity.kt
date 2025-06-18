128521c41045461433d6ebbaf12445b1fb3f9674
Sufia Simjee <sufia123@gmail.com>
2025-06-18 18:54:20 +0630

    Window resize in menifest

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.quizapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.TextUtils
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.btnStart.setOnClickListener {
+            var userName = binding.etName.text.toString()
+            if(!TextUtils.isEmpty(userName)) {
+
+            } else {
+                binding.etName.error = "Please enter a name"
+                binding.etName.requestFocus()
+            }
+        }
+
+        setContentView(binding.root)
     }
 }
