4cd6a775ad1c55f08da32f300c06e1277c22e7a2
Sufia Simjee <sufia123@gmail.com>
2025-06-19 09:54:15 +0630

    Main activity and ui and models created

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.quizapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.example.quizapp.databinding.ActivityMainBinding
+import com.example.quizapp.ui.Question
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +17,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         binding.btnStart.setOnClickListener {
-            var userName = binding.etName.text.toString()
-            if(!TextUtils.isEmpty(userName)) {
 
+            var userName = binding.etName.text.toString()
+
+            if(!TextUtils.isEmpty(userName)) {
+                Intent(this@MainActivity , Question::class.java).also {
+                    startActivity(it)
+                    finish() // close the current activity
+                }
             } else {
                 binding.etName.error = "Please enter a name"
                 binding.etName.requestFocus()
