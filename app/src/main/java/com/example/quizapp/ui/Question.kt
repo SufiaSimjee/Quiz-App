538dc190cd6ed65e18a47157a223406258387469
Sufia Simjee <sufia123@gmail.com>
2025-06-19 14:15:24 +0630

    functions for textview

--- app/src/main/java/com/example/quizapp/ui/Question.kt
+++ app/src/main/java/com/example/quizapp/ui/Question.kt
@@ -1,9 +1,15 @@
 package com.example.quizapp.ui
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
+import android.view.TextureView
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.quizapp.R
 import com.example.quizapp.databinding.ActivityQuestionBinding
 import com.example.quizapp.model.Question
 import com.example.quizapp.utils.Constants
@@ -27,6 +33,12 @@ class Question : AppCompatActivity() , View.OnClickListener {
 
         setQuestion()
 
+        binding.textViewOptionOne.setOnClickListener(this)
+        binding.textViewOptionTwo.setOnClickListener(this)
+        binding.textViewOptionThree.setOnClickListener(this)
+        binding.textViewOptionFour.setOnClickListener(this)
+        binding.buttonCheck.setOnClickListener(this)
+
         setContentView(binding.root)
     }
 
@@ -48,10 +60,57 @@ class Question : AppCompatActivity() , View.OnClickListener {
         }
     }
 
-    override fun onClick(view: View?) {
-        when(view?.id) {
+    private fun resetOption() {
+        val options = mutableListOf<TextView>()
+        options.add(binding.textViewOptionOne)
+        options.add(binding.textViewOptionTwo)
+        options.add(binding.textViewOptionThree)
+        options.add(binding.textViewOptionFour)
 
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this , R.drawable.border_default_option
+            )
         }
     }
+
+    private fun selectedOption(textView: TextView , selectedOptionNumber : Int) {
+        resetOption()
+
+        selectedOptionPosition = selectedOptionNumber
+
+        textView.setTextColor(Color.parseColor("#363A43"))
+        textView.setTypeface(textView.typeface , Typeface.BOLD)
+        textView.background = ContextCompat.getDrawable(
+            this , R.drawable.select_default_option
+        )
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            binding.textViewOptionOne.id -> {
+                selectedOption(binding.textViewOptionOne, 1)
+            }
+
+            binding.textViewOptionTwo.id -> {
+                selectedOption(binding.textViewOptionTwo, 2)
+            }
+
+            binding.textViewOptionThree.id -> {
+                selectedOption(binding.textViewOptionThree, 3)
+            }
+
+            binding.textViewOptionFour.id -> {
+                selectedOption(binding.textViewOptionFour, 4)
+            }
+
+            binding.buttonCheck.id -> {
+
+            }
+        }
+    }
+
 
 }
