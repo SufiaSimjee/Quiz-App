6ebc0a55dcc7b053cd1d2aa964b199a6ed1a88b0
Sufia Simjee <sufia123@gmail.com>
2025-06-20 14:07:48 +0630

    Code complete

--- app/src/main/java/com/example/quizapp/ui/Result.kt
+++ app/src/main/java/com/example/quizapp/ui/Result.kt
@@ -1,10 +1,12 @@
 package com.example.quizapp.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.quizapp.MainActivity
 import com.example.quizapp.R
 import com.example.quizapp.databinding.ActivityResultBinding
 import com.example.quizapp.utils.Constants
@@ -24,6 +26,10 @@ class Result : AppCompatActivity() {
         val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
         binding.textViewScore.text = "You scored $score out of $totalQuestions"
 
+        binding.buttonFinish.setOnClickListener {
+            val intent = Intent(this , MainActivity::class.java)
+            startActivity(intent)
+        }
 
         setContentView(binding.root)
 
